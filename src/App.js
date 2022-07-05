f88b1b18bf31d6174da6127700365995e2117043
Aelida <lidasouza89@gmail.com>
2022-07-04 23:37:49 -0300

    Inclusão Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return (<p>TrybeTunes 1</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route path="/" exact><Login /></Route>
+          <Route path="/search"><Search /></Route>
+          <Route path="/album/:id"><Album /></Route>
+          <Route path="/favorites"><Favorites /></Route>
+          <Route path="/profile" exact><Profile /></Route>
+          <Route path="/profile/edit"><ProfileEdit /></Route>
+          <Route path="*"><NotFound /></Route>
+        </Switch>
+      </>
+
+    );
   }
 }
 
